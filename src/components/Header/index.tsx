2f122dacd6abb083aac3b63296233549d04d427e
NicholasNeto <nicholasneto@outlook.com>
2021-06-18 23:01:45 -0300

    add logo in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
 import { Flex } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react"
 
 
 export function Header() {
     return (
-        <Flex as='header'>
-            <img src="/images/logo.svg" alt="logo word trip" />
+        <Flex
+            display='flex'
+            justifyContent='center'
+            as='header'
+            maxWidth={1440}
+        >
+            <Image
+                boxSize="100px"
+                src="/images/logo.svg"
+                alt="logo word trip"
+            />
         </Flex>
     )
 }
