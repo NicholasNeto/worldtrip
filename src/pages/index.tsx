2cd47a48b50343878d55f620167cfd1e9eebc1ec
NicholasNeto <nicholasneto@outlook.com>
2021-06-19 13:09:34 -0300

    show Banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import {Header } from '../components/Header'
+import { Header } from '../components/Header'
+import { Banner } from '../components/Banner'
 
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Banner />
+    </>
   )
 }
