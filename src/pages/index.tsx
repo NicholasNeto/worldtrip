f4737b4ae7ead8d4b3e950c4ca7ef677083c32d3
NicholasNeto <nicholasneto@outlook.com>
2021-06-20 10:49:08 -0300

    used component TravelType

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../components/Header'
 import { Banner } from '../components/Banner'
+import { TravelType } from '../components/Travel'
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Header />
       <Banner />
+      <TravelType />
     </>
   )
 }
