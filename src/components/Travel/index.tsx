231d8cdaca69d074fe803084351b9ca0b78eaaa1
NicholasNeto <nicholasneto@outlook.com>
2021-06-20 23:14:38 -0300

    add types in Travel

--- src/components/Travel/index.tsx
+++ src/components/Travel/index.tsx
@@ -1,9 +1,51 @@
-import { Flex } from "@chakra-ui/react";
+
+import { HStack, Flex, Box, Image, Text } from "@chakra-ui/react";
 
 export function TravelType() {
     return (
-        <Flex>
-            TravelType
-        </Flex>
+        <HStack
+            spacing={40}
+            justifyContent='center'
+            position='absolute'
+            width='1160px'
+            height='145px'
+            left='140px'
+            top='549px'
+        >
+            <Box>
+                <Image
+                    src="/images/nightlife.svg"
+                />
+                <Text>vida noturna</Text>
+            </Box>
+
+            <Box>
+                <Image
+                    src="/images/beach.svg"
+                />
+                <Text>praia</Text>
+            </Box>
+            <Box>
+                <Image
+                    src="/images/building.svg"
+                />
+                <Text>moderno</Text>
+            </Box>
+            <Box>
+                <Image
+                    src="/images/museum.svg"
+                />
+                <Text>clássico</Text>
+            </Box>
+
+            <Box>
+                <Image
+                    src="/images/earth.svg"
+                />
+                <Text>e mais...</Text>
+            </Box>
+
+
+        </HStack>
     )
 }
