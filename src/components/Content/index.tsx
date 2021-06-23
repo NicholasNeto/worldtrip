25e408516b53ba056939c9aaead9abb502a8b55c
NicholasNeto <nicholasneto@outlook.com>
2021-06-22 22:34:49 -0300

    add Divider in content

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,21 +1,28 @@
-import { VStack, StackDivider, StackItem, Text } from "@chakra-ui/react";
+import { VStack, Center, Divider, Text } from "@chakra-ui/react";
 
 export function Content() {
     return (
+        <>
+            <Divider
+                colorScheme='blackAlpha'
+                size='8'
+                variant='solid'
+            // border='2px solid #47585B'
+            //orientation="horizontal" 
+            />
+            <VStack
+                spacing={4}
+                align="center"
 
-        <VStack
-            spacing={4}
-            align="center"
-            marginTop="10"
-
-            font-family='Poppins'
-            font-style='normal'
-            font-weight='500'
-            font-size='36px'
-            line-height='54px'
-        >
-            <Text>Vamos nessa?</Text>
-            <Text>Então escolha seu continente</Text>
-        </VStack>
+                font-family='Poppins'
+                font-style='normal'
+                font-weight='500'
+                font-size='36px'
+                line-height='54px'
+            >
+                <Text>Vamos nessa?</Text>
+                <Text>Então escolha seu continente</Text>
+            </VStack>
+        </>
     )
 }
