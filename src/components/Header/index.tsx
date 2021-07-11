b8992269770c64df33250b458766041435a0586b
NicholasNeto <nicholasneto@outlook.com>
2021-07-11 20:46:46 -0300

    clean Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,11 @@
-import { Flex, Box, useBreakpointValue } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react"
 
 
 export function Header() {
     return (
         <Flex w={1440} h={100} mx='auto' justifyContent='center' mt='4'>
-            <Image
-                src="/images/logo.svg"
-                alt="logo word trip"
-            />
-            {/* <Flex
-                as='header'
-                mx='auto' // mx margin horizontal | left and right
-                px='6' // px padding horizontal | left and right
-                mt='4'
-                align='center'
-            >
-
-            </Flex> */}
-
+            <Image src="/images/logo.svg" alt="logo word trip" />
         </Flex>
     )
 }
