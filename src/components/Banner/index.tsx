d4358cc1e0be2394b0e115109dbd88dc0317f007
NicholasNeto <nicholasneto@outlook.com>
2021-07-12 23:10:53 -0300

    fix Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,58 +1,63 @@
-import { Image, Flex, Text } from "@chakra-ui/react"
+import { Image, Flex, Text, Box, useBreakpointValue, VStack } from "@chakra-ui/react"
+import React from "react"
 
 export function Banner() {
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
+
     return (
-        <Flex>
-            <Image
-                htmlHeight='368.21px'
-                width='100%'
-                blendMode='normal'
-                src="/images/banner.svg"
-            />
-            <Flex>
-                <Text
-                    position='absolute'
-                    width='426px'
-                    height='108px'
-                    left='140px'
-                    top='180px'
-
-                    color='#F5F8FA'
-                    fontFamily='Poppins'
-                    fontStyle='normal'
-                    fontWeight='500'
-                    fontSize='36px'
-                    lineHeight='54px'
+        <Box
+            bgSize={1}
+            backgroundImage="url('/images/background.jpg')"
+            height={[375, 335]}
+            maxWidth={1440}
+        >
+            <VStack spacing={4} align="flex-start" ml='9rem'>
+                <Box
+                    h={[58, 108]}
+                    w={[238, 426]}
+                    lineHeight={['30px', '54px']}
+                    mt='5rem'
                 >
-                    5 Continentes, infinitas possibilidades
-                </Text>
-                <Text
+                    <Text
+                        fontFamily='Poppins'
+                        fontStyle='normal'
+                        fontWeight='500'
+                        fontSize={[20, 36]}
+                        color='#F5F8FA'
+                    >
+                        5 Continentes, infinitas possibilidades
+                    </Text>
+                </Box>
+                <Box h={[41, 57]} w={[333, 524]} lineHeight={['21px', '30px']} >
+                    <Text
+                        fontFamily='Poppins'
+                        fontStyle='normal'
+                        fontWeight='normal'
+                        fontSize={[14, 20]}
+                        color='#DADADA'
+                    >
+                        Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+                    </Text>
+                </Box>
+
+            </VStack>
+
+            {isWideVersion &&
+                <Image
                     position='absolute'
-                    width='524px'
-                    height='57px'
-                    left='140px'
-                    top='308px'
-
-                    fontFamily='Poppins'
-                    fontStyle='normal'
-                    fontWeight='normal'
-                    fontSize='20px'
-                    line-height='30px'
-                    color='#DADADA'
-                >
-                    Chegou a hora de tirar do papel a viagem que você sempre sonhou.
-                </Text>
-            </Flex>
-
-            <Image
-                position='absolute'
-                width='417.15px'
-                height='270.74px'
-                left='883.42px'
-                top='210px'
-                src="/images/airplane.svg"
-                transform='rotate(3deg)'
-            />
-        </Flex >
+                    width='417.15px'
+                    height='270.74px'
+                    left='883.42px'
+                    top='210px'
+                    src="/images/airplane.svg"
+                    transform='rotate(3deg)'
+                />
+            }
+        </Box >
     )
 }
