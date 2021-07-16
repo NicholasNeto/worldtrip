ff45709abad09886a1041f4ad42eb4739d5519a6
NicholasNeto <nicholasneto@outlook.com>
2021-07-15 21:16:48 -0300

    refactoring banner add maxWidh for Box

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Flex, Text, Box, useBreakpointValue, VStack } from "@chakra-ui/react"
+import { Image, Flex, Text, Box, useBreakpointValue, VStack, Stack } from "@chakra-ui/react"
 import React from "react"
 
 export function Banner() {
@@ -11,7 +11,7 @@ export function Banner() {
             height={['163px', '250px', '250px', '335px']}
             width='100%'
         >
-            <VStack spacing={4} align="flex-start" ml={['1rem', '9rem']}>
+            <Stack direction="column" spacing={10} align="flex-start" ml={['1rem', '9rem']} width="100%">
                 <Box
                     h={['58px', '82px', '82px', '108px']}
                     w={['238px', '285px', '332px', '426px']}
@@ -21,7 +21,7 @@ export function Banner() {
                     <Text
                         fontFamily='Poppins'
                         fontStyle='normal'
-                        fontWeight='400'
+                        fontWeight='500'
                         fontSize={['20px', '28px', '28px', '36px']}
                         color='#F5F8FA'
                     >
@@ -30,20 +30,21 @@ export function Banner() {
                 </Box>
                 <Box
                     h={['41px', '45px', '49px', '57px']}
-                    w={['333px', '380px', '427px', '524px']}
+                    //w={['333px', '380px', '427px', '524px']}
+                    maxW={["100%", "100%", "100%", "550px"]}
                     lineHeight={['21px', '23px', '23px', '30px']}
-                    mt={['0,5rem', '1.25rem']}>
+                >
                     <Text
                         fontFamily='Poppins'
                         fontStyle='normal'
-                        fontWeight='normal'
+                        fontWeight='400'
                         fontSize={['14px', '16px', '18px', '20px']}
                         color='#DADADA'
                     >
                         Chegou a hora de tirar do papel a viagem que você sempre sonhou.
                     </Text>
                 </Box>
-            </VStack>
+            </Stack>
         </Flex >
     )
 }
