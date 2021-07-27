5cddfcd7a22243115b3d17335a68c3d67972fb01
NicholasNeto <nicholasneto@outlook.com>
2021-07-27 00:43:16 -0300

    started created new Slider

---
+++ src/components/Swiper/Slider.tsx
@@ -0,0 +1,16 @@
+
+import { Flex, Heading, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
+
+export function Slider() {
+    return (
+        <Flex>
+
+        </Flex>
+    )
+}
