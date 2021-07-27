6f280931b50c3403c9a6b6d81aa03e33fbd7a2fe
NicholasNeto <nicholasneto@outlook.com>
2021-07-27 01:07:51 -0300

    need created continents in fauna db

--- src/components/Swiper/Slider.tsx
+++ src/components/Swiper/Slider.tsx
@@ -9,8 +9,38 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
 export function Slider() {
     return (
-        <Flex>
-
+        <Flex
+            w="100%"
+            h={["250px", "450px"]}
+            mx="auto"
+            my={["5", "10", "20"]}
+            maxW={'1240px'}
+        >
+            <Swiper
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                autoplay={{
+                    delay: 4000,
+                }}
+                style={{ width: '100%', flex: '1' }}>
+                <SwiperSlide>
+                    <Flex
+                        backgroundImage="url('/images/continents-europa.jpg')"
+                        bgRepeat='no-repeat'
+                        bgSize='cover'
+                        h='100%'
+                        w='100%' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Flex
+                        backgroundImage="url('/images/continents-europa.jpg')"
+                        bgRepeat='no-repeat'
+                        bgSize='cover'
+                        h='100%'
+                        w='100%' />
+                </SwiperSlide>
+            </Swiper>
         </Flex>
     )
 }
