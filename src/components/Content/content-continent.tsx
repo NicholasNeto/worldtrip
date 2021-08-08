918942753167594696d502a67c5a9d6232ec4810
NicholasNeto <nicholasneto@outlook.com>
2021-08-08 20:54:49 -0300

    created component Content for continent

---
+++ src/components/Content/content-continent.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Flex, Grid, GridItem } from "@chakra-ui/react";
+import Infos from "./Infos";
+import { InfosProps } from "../../pages/continent/[slug]";
+
+interface ContentContinentProps {
+    description: string;
+    infos: InfosProps;
+}
+
+
+export function ContentContinent({ description, infos }: ContentContinentProps) {
+    return (
+        <Flex direction="column" maxW="1160px" mx="auto" mb="10" mt='10' px="1rem">
+            <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                <GridItem>{description}</GridItem>
+                <GridItem>
+                    <Infos continent={infos}/>
+                </GridItem>
+            </Grid>
+        </Flex>
+    )
+}
