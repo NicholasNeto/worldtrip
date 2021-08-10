ee2dbea35785751f758d1ed55ce5fdd2175d12b2
NicholasNeto <nicholasneto@outlook.com>
2021-08-09 21:47:15 -0300

    add components Infos responsive

--- src/components/Content/content-continent.tsx
+++ src/components/Content/content-continent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Text, useBreakpointValue } from "@chakra-ui/react";
 import Infos from "./Infos";
 import { InfosProps } from "../../pages/continent/[slug]";
 
@@ -10,13 +10,30 @@ interface ContentContinentProps {
 
 
 export function ContentContinent({ description, infos }: ContentContinentProps) {
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        md: true,
+    })
+
+
     return (
         <Flex direction="column" maxW="1160px" mx="auto" mb="10" mt='10' px="1rem">
-            <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-                <GridItem>{description}</GridItem>
-                {/* <GridItem>
-                    <Infos continent={infos}/>
-                </GridItem> */}
+            <Grid templateColumns={isWideVersion ? "repeat(2, 1fr)" : "repeat(1, 1fr)"} gap={6}>
+                <GridItem>
+                    <Text
+                        fontFamily='Poppins'
+                        fontStyle='normal'
+                        fontWeight='normal'
+                        fontSize={['14px', '20px', '20px', '24px']}
+                        color='#47585B'
+                        textAlign="justify"
+                    >
+                        {description}
+                    </Text>
+                </GridItem>
+                <Infos continent={infos} />
+                {/* <GridItem></GridItem> */}
             </Grid>
         </Flex>
     )
