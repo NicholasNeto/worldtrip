0fa1a721002cdca6423115436c1b609aee72917b
NicholasNeto <nicholasneto@outlook.com>
2021-08-15 20:06:49 -0300

    change city

--- src/components/Cities/index.tsx
+++ src/components/Cities/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Text, Image, VStack, Box } from "@chakra-ui/react";
+import { Flex, Text, Image, Stack, Box } from "@chakra-ui/react";
 import { CitiesProps } from "../../pages/continent/[slug]";
 
 interface CitieProps {
@@ -10,7 +10,7 @@ interface CitieProps {
 export function Cities({ cities }: CitieProps) {
     debugger
     return (
-        <Flex direction="column" maxW="1160px" mx="auto" mb="10" mt='10' px="1rem">
+        <Flex direction="column" maxW="1160px" maxHeight='700' mx="auto" mb="10" mt='10' px="1rem">
             <Text
                 fontFamily='Poppins'
                 fontStyle='normal'
@@ -19,18 +19,17 @@ export function Cities({ cities }: CitieProps) {
                 color='#47585B'
             >Cidades + 100</Text>
 
-            <VStack
-                width="256px"
-                height="279px"
-                borderRadius="4px"
-                border='1px solid rgba(255, 186, 8, 0.5)'
-                backgroundColor='#FFFFFF'
-            >
+            <Stack direction={["column", "row"]} spacing="24px" >
                 {cities.map(it => {
                     return (
-                        <div key={it.city_flag}>
+                        <Box
+                            key={it.city_flag}
+                            borderRadius="4px"
+                            border='1px solid rgba(255, 186, 8, 0.5)'
+                            backgroundColor='#FFFFFF'
+                            height="279px"
+                        >
                             <Flex width="256px" height="173px" backgroundImage={`url(${it.city_image})`} />
-
                             <Flex width='100%' justifyContent="space-around" align='center'>
                                 <Box>
                                     <Text>{it.city_capital}</Text>
@@ -44,11 +43,11 @@ export function Cities({ cities }: CitieProps) {
                                     />
                                 </Box>
                             </Flex>
-                        </div>
+                        </Box >
                     )
                 })}
 
-            </VStack>
+            </Stack>
 
         </Flex>
     )
