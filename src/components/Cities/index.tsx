221776bf4ccfb2dd421c68cdddde8f8e2f85c1e2
NicholasNeto <nicholasneto@outlook.com>
2021-08-15 21:27:06 -0300

    created view for table

--- src/components/Cities/index.tsx
+++ src/components/Cities/index.tsx
@@ -38,32 +38,34 @@ export function Cities({ cities }: CitieProps) {
                         <Flex
                             key={it.city_pais}
                             direction="column"
-                            
-                            width={["256px", '456px']} 
-                            height={["279px",  "379px"]}
-                             
+
+                            width={["256px", '456px', '656px', "256px"]}
+                            height={["279px", "379px", "579px", "279px"]}
+
                             borderRadius="4px"
                             border='1px solid rgba(255, 186, 8, 0.5)'
                             backgroundColor='#FFFFFF'
 
                         >
                             <Flex
-                              width='100%'
-                              height="173px"
-                              backgroundImage={`url(${it.city_image})`} 
+                                width='100%'
+                                height={["173px", "273px", "379px", "173px"]}
+                                backgroundImage={`url(${it.city_image})`}
+                                bgRepeat='no-repeat'
+                                bgSize='cover'
+                                marginBottom={'1rem'}
                             />
                             <Flex width='100%' justifyContent="space-around" align='center'>
-                                <Box>
+                                <Flex direction="column">
                                     <Text>{it.city_capital}</Text>
                                     <Text>{it.city_pais}</Text>
-                                </Box>
-                                <Box>
-                                    <Image
-                                        boxSize="30px"
-                                        borderRadius="full"
-                                        src={it.city_flag}
-                                    />
-                                </Box>
+                                </ Flex>
+
+                                <Image
+                                    boxSize="30px"
+                                    borderRadius="full"
+                                    src={it.city_flag}
+                                />
                             </Flex>
                         </Flex >
                     )
