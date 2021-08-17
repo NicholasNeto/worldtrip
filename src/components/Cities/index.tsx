d8def0f0794a43c9b6a6000a7ecd69fa27c36476
NicholasNeto <nicholasneto@outlook.com>
2021-08-16 22:29:13 -0300

    used new component City

--- src/components/Cities/index.tsx
+++ src/components/Cities/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Flex, Text, Image, SimpleGrid } from "@chakra-ui/react";
+import { Flex, Text, SimpleGrid } from "@chakra-ui/react";
 import { CitiesProps } from "../../pages/continent/[slug]";
+import { City } from './City'
 
 interface CitieProps {
     cities: CitiesProps[]
@@ -26,56 +27,11 @@ export function Cities({ cities }: CitieProps) {
             <SimpleGrid columns={[1, null, null, 4]} spacing="40px" width="100%" >
                 {cities.map(it => {
                     return (
-                        <Flex
-                            key={it.city_pais}
-                            direction="column"
-
-                            width={["256px", '456px', '656px', "256px"]}
-                            height={["279px", "379px", "579px", "279px"]}
-
-                            borderRadius="4px"
-                            border='1px solid rgba(255, 186, 8, 0.5)'
-                            backgroundColor='#FFFFFF'
-
-                        >
-                            <Flex
-                                width='100%'
-                                height={["173px", "273px", "379px", "173px"]}
-                                backgroundImage={`url(${it.city_image})`}
-                                bgRepeat='no-repeat'
-                                bgSize='cover'
-                                marginBottom={'1rem'}
-                            />
-                            <Flex width='100%' justifyContent="space-around" align='center'>
-                                <Flex direction="column">
-                                    <Text
-
-                                        fontFamily='Barlow'
-                                        fontStyle='normal'
-                                        fontWeight='600'
-                                        fontSize={['20px', '30px', '30px', '40px']}
-                                        color='#47585B'
-                                    >
-                                        {it.city_capital}
-                                    </Text>
-                                    <Text
-                                        fontFamily='Barlow'
-                                        fontStyle='normal'
-                                        fontWeight='500'
-                                        fontSize={['16px', '28px']}
-                                        color='#999999'
-                                    >
-                                        {it.city_pais}
-                                    </Text>
-                                </ Flex>
-
-                                <Image
-                                    boxSize="30px"
-                                    borderRadius="full"
-                                    src={it.city_flag}
-                                />
-                            </Flex>
-                        </Flex >
+                        <City
+                            city_pais={it.city_pais}
+                            city_image={it.city_image}
+                            city_flag={it.city_flag}
+                            city_capital={it.city_capital} />
                     )
                 })}
             </SimpleGrid>
